f3ea80d3da9d80fae6ba801e88a3a0aa41ee6c49
jcgsr <cabralguerra@gmail.com>
2020-06-27 07:09:35 -0300

    some missing

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function dim() {
 }
 document.querySelector('#dim').addEventListener('click', dim)
 
-function dim() {
+function mult() {
 	let m1 = Number(document.querySelector('#m1').value)
-	let dd2 = Number(document.querySelector('#m2').value)
+	let m2 = Number(document.querySelector('#m2').value)
 
 	document.querySelector('#mul-resultado').innerHTML = m1 * m2
 	return true
 }
-document.querySelector('#mul').addEventListener('click', dim)
+document.querySelector('#mul').addEventListener('click', mult)
 
 
 function div() {
@@ -51,3 +51,43 @@ function cubo() {
 }
 document.querySelector('#cub').addEventListener('click', cubo)
 
+function pot(){
+	let p1 = Number(document.querySelector("#p1").value)
+	let p2 = Number(document.querySelector("#p2").value)
+
+	document.querySelector('#pot-resultado').innerHTML = Math.pow(p1, p2)
+}
+document.querySelector('#pot').addEventListener('click', pot)
+
+function primo(){
+  let numero = parseInt(document.getElementById('pr1').value);
+  let resposta = document.getElementById('primo-resultado');
+  let divisores=0;
+
+  for(let count=1 ; count<=numero ; count++)
+   if(numero % count == 0)
+   	divisores++;
+  
+  if(divisores==2)
+  	resposta.innerHTML='É PRIMO!';
+  else
+  	resposta.innerHTML='NÃO É PRIMO!';
+}
+document.getElementById('pri').addEventListener('click', primo)
+
+function porcento(){
+	let num = Number(document.querySelector('#per').value)
+	let val = Number(document.querySelector('#val').value)
+
+	document.querySelector('#porcen-resultado').innerHTML = (num * val) / 100
+	return true
+}
+document.querySelector('#porcen').addEventListener('click', porcento)
+
+function raiz() {
+	let r1 = Number(document.querySelector('#r1').value)	
+
+	document.querySelector('#raiz-resultado').innerHTML = Math.sqrt(r1)
+	return true
+}
+document.querySelector('#raiz').addEventListener('click', raiz)
